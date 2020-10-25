c1d8680121e522d72bfd55c4983970c7d3ae2157
Rex Hong <rexhong19871011@gmail.com>
2020-10-26 01:53:18 +1100

    update version number in the readme

--- ToyRobotChallenge/Program.cs
+++ ToyRobotChallenge/Program.cs
@@ -2,7 +2,6 @@
 using System.Collections.Generic;
 using System.IO;
 using System.Linq;
-using System.Text;
 using ToyRobotChallenge.Core.Robot;
 using ToyRobotChallenge.Core.Simulator;
 using ToyRobotChallenge.Core.Table;
